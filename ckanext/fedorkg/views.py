fd9d40aa6e0319d09a385f6ec2b6ed0d66a94285
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-02-13 13:53:12 +0100

    forward queries to DeTrusty

--- ckanext/fedorkg/views.py
+++ ckanext/fedorkg/views.py
@@ -1,14 +1,32 @@
 
-from flask import Blueprint, render_template
+from DeTrusty import run_query
+from DeTrusty.Molecule.MTManager import get_config
+from flask import Blueprint, render_template, jsonify, request
 
 fedorkg = Blueprint('fedorkg', __name__, url_prefix='/fedorkg')
 
 
-def sparql():
+def query_editor():
     return render_template('sparql.jinja2')
 
 
-fedorkg.add_url_rule('/sparql', view_func=sparql)
+def sparql():
+    query = request.values.get('query', None)
+    if query is None:
+        return jsonify({"result": [], "error": "No query passed."})
+    yasqe = request.values.get('yasqe', False)
+    return jsonify(
+        run_query(
+            query=query,
+            config=get_config('/DeTrusty/Config/rdfmts.json'),
+            join_stars_locally=False,
+            yasqe=yasqe
+        )
+    )
+
+
+fedorkg.add_url_rule('/sparql', view_func=query_editor, methods=['GET'])
+fedorkg.add_url_rule('/sparql', view_func=sparql, methods=['POST'])
 
 
 def get_blueprints():
