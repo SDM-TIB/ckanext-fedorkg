193f8e02618b6bbf2d3e9c0fe51ec27bd8c4e64c
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-02-13 09:23:14 +0100

    use template for content

--- ckanext/fedorkg/views.py
+++ ckanext/fedorkg/views.py
@@ -1,14 +1,14 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-fedorkg = Blueprint('fedorkg', __name__)
+fedorkg = Blueprint('fedorkg', __name__, url_prefix='/fedorkg')
 
 
 def sparql():
-    return 'Hello, this is FedORKG!'
+    return render_template('sparql.jinja2')
 
 
-fedorkg.add_url_rule('/fedorkg/sparql', view_func=sparql)
+fedorkg.add_url_rule('/sparql', view_func=sparql)
 
 
 def get_blueprints():
