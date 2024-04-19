16b218a1526031db56896b756c0071d431a5b498
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-04-19 11:55:55 +0200

    add dummy for adding a KG to the federation

--- ckanext/fedorkg/controller.py
+++ ckanext/fedorkg/controller.py
@@ -24,17 +24,27 @@ class FedORKGController:
 
         from ckanext.fedorkg.views import detrusty_config
         msg = ''
-        error = True
+        error = False
         if request.method == 'POST':
             kg = request.form.get('kg')
-            # TODO: Try to delete the KG from the federation and set error state accordingly
-            if error:
-                msg = toolkit._('There was an error when deleting {kg} from the federation!').format(kg=kg)
-            else:
-                msg = toolkit._('Successfully removed {kg} from the federation!').format(kg=kg)
-            # TODO: Remove after implementing the deletion of a KG from the federation
-            error = True
-            msg = 'This feature is not yet implemented.'
+            action = request.form.get('action')
+            if action == '0':
+                # TODO: Try to delete the KG from the federation and set error state accordingly
+                if error:
+                    msg = toolkit._('There was an error when deleting {kg} from the federation!').format(kg=kg)
+                else:
+                    msg = toolkit._('Successfully removed {kg} from the federation!').format(kg=kg)
+                # TODO: Remove after implementing the deletion of a KG from the federation
+                error = True
+                msg = 'This feature is not yet implemented.'
+            elif action == '1':
+                # TODO: Try to add the KG to the federation and set error state accordingly
+                if error:
+                    msg = toolkit._('There was an error when adding {kg} from the federation!').format(kg=kg)
+                else:
+                    msg = toolkit._('Successfully added {kg} from the federation!').format(kg=kg)
+                error = True
+                msg = 'This feature is not yet implemented.'
         return toolkit.render('admin.jinja2',
                               extra_vars={
                                   'kgs': sorted(list(detrusty_config.getEndpoints().keys())),
