c97e20a41353c47ff4bba01c1d435fef1147c5e1
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-04-19 14:59:22 +0200

    fix admin page marked as active FedORKG page

--- ckanext/fedorkg/helpers.py
+++ ckanext/fedorkg/helpers.py
@@ -3,4 +3,4 @@ import ckan.lib.helpers as h
 
 
 def is_fedorkg_page():
-    return '/fedorkg' in h.current_url()
+    return False if 'ckan-admin' in h.current_url() else '/fedorkg' in h.current_url()  # exclude the admin page
