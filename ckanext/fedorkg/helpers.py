2148c4d971894929e4067283df50e54d843d4a38
Philipp Rohde <Philipp.Rohde@tib.eu>
2025-08-05 16:10:41 +0200

    exclude dashboard page from nav highlight

--- ckanext/fedorkg/helpers.py
+++ ckanext/fedorkg/helpers.py
@@ -4,7 +4,9 @@ import ckan.plugins.toolkit as toolkit
 
 
 def is_fedorkg_page():
-    return False if 'ckan-admin' in h.current_url() else '/fedorkg' in h.current_url()  # exclude the admin page
+    # exclude the admin and dashboard page
+    return False if 'ckan-admin' in h.current_url() or 'dashboard' in h.current_url() else '/fedorkg' in h.current_url()
+
 
 def icon():
     if toolkit.check_ckan_version(min_version='2.10'):
